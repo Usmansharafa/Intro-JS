f0a71d4b7447a9308e9a358833efc9f002bd93d0
Usman Sharafa <103945182+Usmansharafa@users.noreply.github.com>
2022-06-20 22:50:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var name = Sharafa Usman
+var name = "Sharafa Usman"
 var height = "5 feet 11 inches"
-var country = Nigeria
-alert(`My name is ${name}. I am ${height} tall. I am from {country}.`)
+var country = "Nigeria"
+alert(`My name is ${name}. I am ${height} tall. I am from ${country}.`)
