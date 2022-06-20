fd914dd67da34bdd38101fb772a9b2b6cf711b2e
Usman Sharafa <103945182+Usmansharafa@users.noreply.github.com>
2022-06-20 21:41:35 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var name = prompt("Enter your name: ")
+var height = prompt("Enter your height: ")
+var country = prompt("Enter your country: ")
+
+console.log(`My name is ${name}. I am ${height} tall. I am from {country}.`)
